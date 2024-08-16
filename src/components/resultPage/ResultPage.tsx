33aaa3ef4716253d9c5e685f5fb13289cdafe582
sunshine-web-coder <webdev4gaming@gmail.com>
2024-08-16 11:41:07 +0100

    new

--- src/components/resultPage/ResultPage.tsx
+++ src/components/resultPage/ResultPage.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState, useCallback, useMemo } from "react";
-import { Package } from "../../utils/types";
-import Image from "next/image";
-import Link from "next/link";
-import { FaUser } from "react-icons/fa";
-import { FaArrowDownShortWide } from "react-icons/fa6";
-import logo from "../../assets/dhl-logo.svg";
+import React, { useEffect, useState, useCallback, useMemo } from 'react';
+import { Package } from '../../utils/types';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaUser } from 'react-icons/fa';
+import { FaArrowDownShortWide } from 'react-icons/fa6';
+import logo from '../../assets/dhl-logo.svg';
 
 interface ResultPageProps {
   packageInfo: Package;
@@ -16,27 +16,27 @@ export default function ResultPage({ packageInfo }: ResultPageProps) {
   const steps = useMemo(() => {
     const stepList = [
       {
-        label: "Package Received By DHL",
-        datetime: `${packageInfo.package_received_date}T${packageInfo.package_received_time}`,
+        label: 'Package Received By DHL',
+        datetime: `${packageInfo.package_received_date}T${packageInfo.package_received_time}`
       },
       {
-        label: "In Transit",
-        datetime: `${packageInfo.in_transit_date}T${packageInfo.in_transit_time}`,
+        label: 'In Transit',
+        datetime: `${packageInfo.in_transit_date}T${packageInfo.in_transit_time}`
       },
       {
-        label: "Out for Delivery",
-        datetime: `${packageInfo.out_for_delivery_date}T${packageInfo.out_for_delivery_time}`,
+        label: 'Out for Delivery',
+        datetime: `${packageInfo.out_for_delivery_date}T${packageInfo.out_for_delivery_time}`
       },
       {
-        label: "Delivered",
-        datetime: `${packageInfo.estimated_delivery_date}T${packageInfo.estimated_delivery_time}`,
-      },
+        label: 'Delivered',
+        datetime: `${packageInfo.estimated_delivery_date}T${packageInfo.estimated_delivery_time}`
+      }
     ];
 
     if (packageInfo.on_hold_date && packageInfo.on_hold_time) {
       stepList.splice(3, 0, {
-        label: "On Hold",
-        datetime: `${packageInfo.on_hold_date}T${packageInfo.on_hold_time}`,
+        label: 'On Hold',
+        datetime: `${packageInfo.on_hold_date}T${packageInfo.on_hold_time}`
       });
     }
 
@@ -69,19 +69,19 @@ export default function ResultPage({ packageInfo }: ResultPageProps) {
 
   const formatDate = (datetime: string) => {
     const options: Intl.DateTimeFormatOptions = {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
     };
     return new Date(datetime).toLocaleDateString(undefined, options);
   };
 
   const formatTime = (datetime: string) => {
     const options: Intl.DateTimeFormatOptions = {
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: true,
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true
     };
     return new Date(datetime).toLocaleTimeString(undefined, options);
   };
@@ -89,15 +89,17 @@ export default function ResultPage({ packageInfo }: ResultPageProps) {
   const getStatus = (step: number) => {
     switch (step) {
       case 0:
-        return "Package Received";
+        return 'Package Received';
       case 1:
-        return "In Transit";
+        return 'In Transit';
       case 2:
-        return "Out for Delivery";
+        return 'Out for Delivery';
       case 3:
-        return "Delivered";
+        return 'On Hold';
+      case 3:
+        return 'Delivered';
       default:
-        return "Processing";
+        return 'Processing';
     }
   };
 
@@ -110,27 +112,20 @@ export default function ResultPage({ packageInfo }: ResultPageProps) {
           </Link>
           <Link href="/">Go Home</Link>
         </div>
-        <div className="p-4 rounded bg-gradient mb-[30px] mt-[30px] text-center font-semibold">
-          Tracking Number: [{packageInfo.tracking_number}] found.
-        </div>
+        <div className="p-4 rounded bg-gradient mb-[30px] mt-[30px] text-center font-semibold">Tracking Number: [{packageInfo.tracking_number}] found.</div>
         <div className="border p-4 md:p-8 rounded-lg">
           <div className="vertical-progress-container">
             {steps.map((step, index) => (
-              <div
-                key={index}
-                className={`step ${index <= currentStep ? "active" : ""}`}
-              >
+              <div key={index} className={`step ${index <= currentStep ? 'active' : ''}`}>
                 <div className="circle"></div>
                 <div className="content">
-                  <p className="label text-base font-semibold uppercase">
-                    {step.label}
-                  </p>
-                  <p className="text-sm flex flex-col mt-1">
-                    {formatDate(step.datetime)}
-                    <span className="text-[13px]">
-                      {formatTime(step.datetime)}
-                    </span>
-                  </p>
+                  <p className="label text-base font-semibold uppercase">{step.label}</p>
+                  {index <= currentStep && (
+                    <p className="text-sm flex flex-col mt-1">
+                      {formatDate(step.datetime)}
+                      <span className="text-[13px]">{formatTime(step.datetime)}</span>
+                    </p>
+                  )}
                 </div>
               </div>
             ))}
@@ -164,18 +159,10 @@ export default function ResultPage({ packageInfo }: ResultPageProps) {
                   </thead>
                   <tbody>
                     <tr className="bg-white text-[#333333] border">
-                      <td className="px-4 py-2 w-[200px]">
-                        {packageInfo.sender.name}
-                      </td>
-                      <td className="px-4 py-2 w-[200px]">
-                        {packageInfo.sender.contact_number}
-                      </td>
-                      <td className="px-4 py-2 w-[300px]">
-                        {packageInfo.sender.address}
-                      </td>
-                      <td className="px-4 py-2 w-[200px]">
-                        {packageInfo.sender.email}
-                      </td>
+                      <td className="px-4 py-2 w-[200px]">{packageInfo.sender.name}</td>
+                      <td className="px-4 py-2 w-[200px]">{packageInfo.sender.contact_number}</td>
+                      <td className="px-4 py-2 w-[300px]">{packageInfo.sender.address}</td>
+                      <td className="px-4 py-2 w-[200px]">{packageInfo.sender.email}</td>
                     </tr>
                   </tbody>
                 </table>
@@ -211,18 +198,10 @@ export default function ResultPage({ packageInfo }: ResultPageProps) {
                   </thead>
                   <tbody>
                     <tr className="bg-white text-[#333333] border">
-                      <td className="px-4 py-2 w-[200px]">
-                        {packageInfo.recipient.name}
-                      </td>
-                      <td className="px-4 py-2 w-[200px]">
-                        {packageInfo.recipient.contact_number}
-                      </td>
-                      <td className="px-4 py-2 w-[300px]">
-                        {packageInfo.recipient.address}
-                      </td>
-                      <td className="px-4 py-2 w-[200px]">
-                        {packageInfo.recipient.email}
-                      </td>
+                      <td className="px-4 py-2 w-[200px]">{packageInfo.recipient.name}</td>
+                      <td className="px-4 py-2 w-[200px]">{packageInfo.recipient.contact_number}</td>
+                      <td className="px-4 py-2 w-[300px]">{packageInfo.recipient.address}</td>
+                      <td className="px-4 py-2 w-[200px]">{packageInfo.recipient.email}</td>
                     </tr>
                   </tbody>
                 </table>
@@ -240,93 +219,59 @@ export default function ResultPage({ packageInfo }: ResultPageProps) {
             <div>
               <div className="bg-[#858585] text-white flex items-center justify-between">
                 <div className="w-full p-3 py-2 border-r">Weight (kg)</div>
-                <div className="w-full p-3 py-2">
-                  {packageInfo.package_details.weight_kg}kg
-                </div>
+                <div className="w-full p-3 py-2">{packageInfo.package_details.weight_kg}kg</div>
               </div>
               <div className="bg-white flex items-center justify-between">
                 <div className="w-full p-3 py-2 border-r">Courier</div>
-                <div className="w-full p-3 py-2">
-                  {packageInfo.package_details.courier}
-                </div>
+                <div className="w-full p-3 py-2">{packageInfo.package_details.courier}</div>
               </div>
               <div className="bg-[#858585] text-white flex items-center justify-between">
                 <div className="w-full p-3 py-2 border-r">Packages</div>
-                <div className="w-full p-3 py-2">
-                  {packageInfo.package_details.packages}
-                </div>
+                <div className="w-full p-3 py-2">{packageInfo.package_details.packages}</div>
               </div>
               <div className="bg-white flex items-center justify-between">
                 <div className="w-full p-3 py-2 border-r">Quantity</div>
-                <div className="w-full p-3 py-2">
-                  {packageInfo.package_details.quantity}
-                </div>
+                <div className="w-full p-3 py-2">{packageInfo.package_details.quantity}</div>
               </div>
               <div className="bg-[#858585] text-white flex items-center justify-between">
                 <div className="w-full p-3 py-2 border-r">Payment Mode</div>
-                <div className="w-full p-3 py-2">
-                  {packageInfo.package_details.paymentMode}
-                </div>
+                <div className="w-full p-3 py-2">{packageInfo.package_details.paymentMode}</div>
               </div>
               <div className="bg-white flex items-center justify-between">
                 <div className="w-full p-3 py-2 border-r">Origin</div>
-                <div className="w-full p-3 py-2">
-                  {packageInfo.package_details.origin}
-                </div>
+                <div className="w-full p-3 py-2">{packageInfo.package_details.origin}</div>
               </div>
               <div className="bg-[#858585] text-white flex items-center justify-between">
                 <div className="w-full p-3 py-2 border-r">Destination</div>
-                <div className="w-full p-3 py-2">
-                  {packageInfo.package_details.destination}
-                </div>
+                <div className="w-full p-3 py-2">{packageInfo.package_details.destination}</div>
               </div>
               <div className="bg-white flex items-center justify-between">
                 <div className="w-full p-3 py-2 border-r">Pickup Date</div>
-                <div className="w-full p-3 py-2">
-                  {formatDate(packageInfo.pickup_date ?? "")}
-                </div>
+                <div className="w-full p-3 py-2">{formatDate(packageInfo.pickup_date ?? '')}</div>
               </div>
               <div className="bg-[#858585] text-white flex items-center justify-between">
                 <div className="w-full p-3 py-2 border-r">Pickup Time</div>
-                <div className="w-full p-3 py-2">
-                  {formatTime(
-                    `${packageInfo.pickup_date}T${packageInfo.pickup_time}`
-                  )}
-                </div>
+                <div className="w-full p-3 py-2">{formatTime(`${packageInfo.pickup_date}T${packageInfo.pickup_time}`)}</div>
               </div>
               <div className="bg-white flex items-center justify-between">
                 <div className="w-full p-3 py-2 border-r">Status</div>
                 <div className="w-full p-3 py-2">{getStatus(currentStep)}</div>
               </div>
               <div className="bg-[#858585] text-white flex items-center justify-between">
-                <div className="w-full p-3 py-2 border-r">
-                  Estimated Delivery Date
-                </div>
-                <div className="w-full p-3 py-2">
-                  {formatDate(packageInfo.estimated_delivery_date)}
-                </div>
+                <div className="w-full p-3 py-2 border-r">Estimated Delivery Date</div>
+                <div className="w-full p-3 py-2">{formatDate(packageInfo.estimated_delivery_date)}</div>
               </div>
               <div className="bg-white flex items-center justify-between">
-                <div className="w-full p-3 py-2 border-r">
-                  Estimated Delivery Time
-                </div>
-                <div className="w-full p-3 py-2">
-                  {formatTime(
-                    `${packageInfo.estimated_delivery_date}T${packageInfo.estimated_delivery_time}`
-                  )}
-                </div>
+                <div className="w-full p-3 py-2 border-r">Estimated Delivery Time</div>
+                <div className="w-full p-3 py-2">{formatTime(`${packageInfo.estimated_delivery_date}T${packageInfo.estimated_delivery_time}`)}</div>
               </div>
               <div className="bg-[#858585] text-white flex items-center justify-between">
                 <div className="w-full p-3 py-2 border-r">Mode</div>
-                <div className="w-full p-3 py-2">
-                  {packageInfo.package_details.mode}
-                </div>
+                <div className="w-full p-3 py-2">{packageInfo.package_details.mode}</div>
               </div>
               <div className="bg-white flex items-center justify-between">
                 <div className="w-full p-3 py-2 border-r">Comment</div>
-                <div className="w-full p-3 py-2">
-                  {packageInfo.package_details.comment}
-                </div>
+                <div className="w-full p-3 py-2">{packageInfo.package_details.comment}</div>
               </div>
             </div>
           </div>
--- src/components/resultPage/mockData/MockData.ts
+++ src/components/resultPage/mockData/MockData.ts
@@ -1,4 +1,4 @@
-import { Package } from "@/utils/types";
+import { Package } from '@/utils/types';
 
 export const mockPackages: Package[] = [
   {
@@ -7,13 +7,13 @@ export const mockPackages: Package[] = [
       name: 'John Doe',
       address: '123 Main St, Springfield, IL, 62701',
       contact_number: '555-1234',
-      email: 'sender@example.com',
+      email: 'sender@example.com'
     },
     recipient: {
       name: 'Jane Smith',
       address: '456 Elm St, Shelbyville, IL, 62565',
       contact_number: '555-5678',
-      email: 'recipient@example.com',
+      email: 'recipient@example.com'
     },
     tracking_number: 'DFCE0085E33',
     package_details: {
@@ -21,7 +21,7 @@ export const mockPackages: Package[] = [
       dimensions_cm: {
         length: 30,
         width: 20,
-        height: 15,
+        height: 15
       },
       courier: 'DHL',
       packages: 'Books',
@@ -31,7 +31,7 @@ export const mockPackages: Package[] = [
       destination: 'Shelbyville, IL',
       comment: 'Handle with care',
       mode: 'Road',
-      value_usd: 50.0,
+      value_usd: 50.0
     },
     package_received_date: '2024-05-23',
     package_received_time: '06:09:00',
@@ -42,7 +42,7 @@ export const mockPackages: Package[] = [
     on_hold_date: '',
     on_hold_time: '',
     estimated_delivery_date: '2024-05-23',
-    estimated_delivery_time: '06:13:00',
+    estimated_delivery_time: '06:13:00'
   },
   {
     package_id: 'PKG002',
@@ -50,13 +50,13 @@ export const mockPackages: Package[] = [
       name: 'Thomas Levi',
       address: 'Abuja Wuse 2',
       contact_number: '******',
-      email: 'thomaslevi020@gmail.com',
+      email: 'thomaslevi020@gmail.com'
     },
     recipient: {
       name: 'Collins Fred',
       address: '298 north street buffalo New York',
       contact_number: '******',
-      email: 'collinsbarrister0@gmail.com',
+      email: 'collinsbarrister0@gmail.com'
     },
     tracking_number: 'DFCE0085E34',
     package_details: {
@@ -64,7 +64,7 @@ export const mockPackages: Package[] = [
       dimensions_cm: {
         length: 30,
         width: 20,
-        height: 15,
+        height: 15
       },
       courier: 'DHL',
       packages: 'Gold',
@@ -75,7 +75,7 @@ export const mockPackages: Package[] = [
       comment: 'Handle with care',
       mode: 'Sea',
       typeOfShipment: 'Ship',
-      value_usd: 50.0,
+      value_usd: 50.0
     },
     package_received_date: '2024-08-13',
     package_received_time: '12:00:00',
@@ -83,11 +83,11 @@ export const mockPackages: Package[] = [
     in_transit_time: '14:00:00',
     out_for_delivery_date: '2024-08-16',
     out_for_delivery_time: '11:00:00',
-    on_hold_date: '',
-    on_hold_time: '',
+    on_hold_date: '2024-08-16',
+    on_hold_time: '12:00:00',
     estimated_delivery_date: '2024-08-17',
     estimated_delivery_time: '12:00:00',
     pickup_date: '2024-08-17',
-    pickup_time: '08:00:00',
-  },
+    pickup_time: '08:00:00'
+  }
 ];
